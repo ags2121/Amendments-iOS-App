0026d6c78bf8e59f8ea49651809c68a73a9a1d5b
Alex Silva <alexsilva88@gmail.com>
2013-02-28 12:08:56 -0600

    Initial Commit

---
+++ Amendments/AmendmentsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AmendmentsAppDelegate.h
+//  Amendments
+//
+//  Created by Alex Silva on 2/28/13.
+//  Copyright (c) 2013 Alex Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AmendmentsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Amendments/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Amendments
+//
+//  Created by Alex Silva on 2/28/13.
+//  Copyright (c) 2013 Alex Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AmendmentsAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AmendmentsAppDelegate class]));
+    }
+}
